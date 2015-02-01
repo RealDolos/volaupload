066519c2beb1315dd023165cd29d0a37af0a0eac
RealDolos <dolosthegreat@safe-mail.net>
2015-02-02 00:44:41 +0100

    No mo socketeering
    Seems to decrease speed sometimes

--- volaupload/__main__.py
+++ volaupload/__main__.py
@@ -7,33 +7,6 @@ import sys
 
 from volaupload import main
 
-def socketeering():
-    """Use larger socket...like... huge"""
-    import socket
-
-    class LargeSocket(socket.socket):
-        """A socket with moar buffer"""
-        # pylint: disable=too-few-public-methods
-
-        options = (
-            (socket.SO_RCVBUF, 1 << 17),
-            (socket.SO_SNDBUF, 1 << 24))
-
-        def __init__(self, *args, **kw):
-            super().__init__(*args, **kw)
-            if self.family == socket.AF_INET:
-                try:
-                    for opt, val in self.options:
-                        cur = self.getsockopt(socket.SOL_SOCKET, opt)
-                        if val > cur:
-                            self.setsockopt(socket.SOL_SOCKET, opt, val)
-                except Exception as ex:
-                    print("Failed to set sockopt", ex,
-                          file=sys.stderr, flush=True)
-
-    socket.socket = LargeSocket
-
-
 def run():
     """ Run as CLI command """
     import codecs
@@ -48,8 +21,6 @@ def run():
     if sys.stderr.encoding.casefold() != "utf-8".casefold():
         sys.stderr = codecs.getwriter(sys.stderr.encoding)(
             sys.stderr.buffer, 'replace')
-
-    socketeering()
 
     sys.exit(main())
 
